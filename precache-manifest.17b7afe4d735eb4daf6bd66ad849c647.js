f4d4ad768010e3c86e8bd2a3cef4be42fd5506cd
sunil pattar <sunilpattar99@gmail.com>
2020-08-01 22:08:16 +0530

    Updates

---
+++ precache-manifest.17b7afe4d735eb4daf6bd66ad849c647.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18b0b08c038f3addbfe857bb1869b047",
+    "url": "/Survey_Sparrow_Assignment/index.html"
+  },
+  {
+    "revision": "b95f198d4b14c4bc1d4b",
+    "url": "/Survey_Sparrow_Assignment/static/js/2.45efd309.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Survey_Sparrow_Assignment/static/js/2.45efd309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb9d51129268df9df6e6",
+    "url": "/Survey_Sparrow_Assignment/static/js/main.d8c8df46.chunk.js"
+  },
+  {
+    "revision": "088d87cfd881f8c07409",
+    "url": "/Survey_Sparrow_Assignment/static/js/runtime-main.0c5ccade.js"
+  }
+]);
